a717fe482b55cac5732dbf422ee02d04d35d785c
redknot <redknotmiaoyuqiao@gmail.com>
2015-11-26 21:19:16 +0800

    xml_struct

--- xml/xml.h
+++ xml/xml.h
@@ -1,4 +1,14 @@
 typedef struct xml_node
 {
     char * name;
+    xml_attr attr[];
+    char * content;
+    struct xml_node * parent;
+    struct xml_node child[];
 } xml_node;
+
+typedef struct xml_attr
+{
+    char * name;
+    char * content;
+} xml_attr;
